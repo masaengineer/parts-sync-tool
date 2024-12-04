b32488168995a4120098235d17140d47e95e7dd5
masaengineer <masahiro.track.a.f@gmail.com>
2024-12-04 21:44:56 +0900

    UIテンプレートを設置

---
+++ User.java
@@ -0,0 +1,10 @@
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+}
---
+++ pages/api/users.ts
@@ -0,0 +1,4 @@
+export default async function handler(req, res) {
+  const users = await prisma.user.findMany()
+  res.json(users)
+}
