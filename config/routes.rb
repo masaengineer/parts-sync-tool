811d6639e93f4b7b9022ae9c0b208198ef466fc0
masaengineer <masahiro.track.a.f@gmail.com>
2024-12-12 19:40:41 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,7 @@
 #
 
 Rails.application.routes.draw do
-  get "users/index"
-  get "users/show"
-  get "users/edit"
-  get "users/update"
-  get "plreports/index"
+  resources :users, only: [:index, :show, :edit, :update]
   resources :plreports, only: [:index]
   root 'landing#index'
 
