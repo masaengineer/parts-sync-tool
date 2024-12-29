08e82cf2d841bdbc396020073993f4769117b073
masaengineer <masahiro.track.a.f@gmail.com>
2024-12-30 02:22:53 +0900

    軽微な修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
     # サインアップ時のパラメータ許可
     devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :email, :password, :password_confirmation, :agreement])
     # アカウント更新時のパラメータ許可
-    devise_parameter_sanitizer.permit(:account_update, keys: [:last_name, :first_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:last_name, :first_name, :email, :password, :password_confirmation, :agreement])
   end
 
   def after_sign_out_path_for(resource_or_scope)
@@ -26,25 +26,5 @@ class ApplicationController < ActionController::Base
     else
       "application"
     end
-  end
-
-  def flash_message(key, resource_name = nil)
-    t(key, resource: resource_name, scope: 'flash_messages')
-  end
-
-  def flash_success(key, resource_name = nil)
-    flash[:success] = flash_message(key, resource_name)
-  end
-
-  def flash_error(key, resource_name = nil)
-    flash[:error] = flash_message(key, resource_name)
-  end
-
-  def flash_notice(key, resource_name = nil)
-    flash[:notice] = flash_message(key, resource_name)
-  end
-
-  def flash_alert(key, resource_name = nil)
-    flash[:alert] = flash_message(key, resource_name)
   end
 end
